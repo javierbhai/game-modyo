a6171417f3341e149a1213110c4a80bebc5e4d90
Javier <javiercardenastobon@gmail.com>
2023-04-21 10:33:41 -0500

    final comit

--- src/components/atoms/Card/index.jsx
+++ src/components/atoms/Card/index.jsx
@@ -4,7 +4,7 @@ const Card = ({ id, isFlipped, handleClick, cardNumber, card }) => (
   <div className='card__two'>
     <div className='card__wrapper'>
       <div id={id} className={`${isFlipped && 'clever'} front`} onClick={handleClick}>
-        ?
+        <i className="fa fa-question-circle" aria-hidden="true"></i>
       </div>
       <div id={id} className={`${isFlipped && 'clever'} back`} onClick={handleClick}>
         {card.imageUrl ? <img id={id} src={card.imageUrl} alt="Animal" /> : <i className="fa fa-heart" aria-hidden="true"></i>}
